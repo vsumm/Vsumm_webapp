7cabde82f55f8dc6d1320512d6b095d283d67ba2
sumiS255@ <sumis2140@gmail.com>
2022-04-09 10:50:04 +0530

    Updates

---
+++ precache-manifest.46bac08249db826d5f0f1b3d4c66fd18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95c7cb969f8cda32cd4cb5c2126d796",
+    "url": "/Vsumm_webapp/index.html"
+  },
+  {
+    "revision": "26e42e970b64a4312902",
+    "url": "/Vsumm_webapp/static/css/2.8647ba78.chunk.css"
+  },
+  {
+    "revision": "2ae7a4ee1364ae9ddd0f",
+    "url": "/Vsumm_webapp/static/css/main.6b676391.chunk.css"
+  },
+  {
+    "revision": "26e42e970b64a4312902",
+    "url": "/Vsumm_webapp/static/js/2.352e9b7f.chunk.js"
+  },
+  {
+    "revision": "98d2c9dcb863f04b9a8f0ae31281550a",
+    "url": "/Vsumm_webapp/static/js/2.352e9b7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae7a4ee1364ae9ddd0f",
+    "url": "/Vsumm_webapp/static/js/main.0c005b51.chunk.js"
+  },
+  {
+    "revision": "d0453014a462226469d6",
+    "url": "/Vsumm_webapp/static/js/runtime-main.a7b22351.js"
+  },
+  {
+    "revision": "220c14ca9dc7462baa7ae1d971dc0f89",
+    "url": "/Vsumm_webapp/static/media/video.220c14ca.mp4"
+  }
+]);
